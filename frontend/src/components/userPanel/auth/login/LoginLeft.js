edb8407824723a38146a6807d2ff5eb56b30c347
mlhnshuvo <mlhnshuvo@gmail.com>
2022-06-01 23:36:14 +0600

    youtube video link add

--- frontend/src/components/userPanel/auth/login/LoginLeft.js
+++ frontend/src/components/userPanel/auth/login/LoginLeft.js
@@ -1,10 +1,16 @@
 import React from "react";
-import LeftImg from "../../../../assets/images/loginleft.jpeg";
 
 const LoginLeft = () => {
   return (
-    <div className="sm:w-3/5 w-full px-5 sm:px-0 mb-10 sm:mb-0">
-      <img src={LeftImg} alt="" />
+    <div className="sm:w-3/5 w-full px-5 sm:px-0 my-10 sm:mb-0 flex justify-center">
+      <iframe
+        className="w-4/5 h-4/5"
+        src="https://www.youtube.com/embed/PzQwrUi7LBw"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
     </div>
   );
 };
--- frontend/src/components/userPanel/auth/register/index.js
+++ frontend/src/components/userPanel/auth/register/index.js
@@ -4,7 +4,7 @@ import Register from "./Register";
 
 const index = () => {
   return (
-    <div className="flex gap-10 mt-12 max-w-7xl m-auto flex-wrap justify-center">
+    <div className="flex gap-2 mt-12 max-w-7xl m-auto flex-wrap justify-center">
       <LeftImg />
       <Register />
     </div>
